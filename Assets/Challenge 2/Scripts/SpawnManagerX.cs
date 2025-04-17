e7fea5c4591b9cc8a21aa981b976cb673f446b0a
Ly_Man <lym861@hsd.k12.or.us>
2025-04-17 10:16:55 -0700

    editing the script make the time of spawning ball is random

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,11 +11,11 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        float spawnInterval = Random.Range(3.0f, 5.0f);
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
