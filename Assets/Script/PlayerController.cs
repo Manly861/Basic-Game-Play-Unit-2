2b46e78a08a37a11de9533edd4d4788c130fa388
Ly_Man <lym861@hsd.k12.or.us>
2025-04-18 12:48:51 -0700

    fixing the script that allow player move forward and backward

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     private float xRange = 10;
+    private float zRange = 15;
     public GameObject projectilePrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,8 +27,18 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
